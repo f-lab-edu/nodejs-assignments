116ee54f8011b048d21af7162b78afe8d940191c
Harry <harry@bzznbyd.com>
2025-07-19 22:35:41 +0900

    feat: GraphQL + REST 하이브리드 프로젝트 초기 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 import { HealthModule } from './health/health.module';
+import { DatabaseModule } from './common/database/database.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true,
+      introspection: true,
+      context: ({ req }) => ({ req }),
+    }),
+    HealthModule,
+  ],
   controllers: [],
   providers: [],
 })
